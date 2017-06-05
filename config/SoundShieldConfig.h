896ca697520bb89195d7fa0954f147f27b8883f3
Sung <sung@Sungs-MacBook-Pro.local>
2017-06-04 22:37:06 -0500

    Removed py35 bytecode. TODO: Downsize ./python/ to core ss components

--- config/SoundShieldConfig.h
+++ config/SoundShieldConfig.h
@@ -2,3 +2,6 @@
 // Initial Creation Date: October 18, 2015
 // Description: Sound Shield Configuration File
 // Date of Last Revision: October 18, 2015
+
+#define SoundShield_VERSION_MAJOR 1 
+#define SoundShield_VERSION_MINOR 
